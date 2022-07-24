4dfd43f25d20ee037a2924eab487b3f164612990
KoushikG@kgs <50265588+koushikgshashidhar@users.noreply.github.com>
2022-07-24 22:47:53 +0530

    Time: 87 ms (87.08%), Space: 116.9 MB (78.57%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.java
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.java
@@ -1,39 +1,51 @@
 public class Solution {
-    public List<Integer> countSmaller(int[] nums) {
-        List<Integer> res = new LinkedList<Integer>();
-        if (nums == null || nums.length == 0) {
-            return res;
-        }
-        // find min value and minus min by each elements, plus 1 to avoid 0 element
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < nums.length; i++) {
-            min = (nums[i] < min) ? nums[i]:min;
-        }
-        int[] nums2 = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            nums2[i] = nums[i] - min + 1;
-            max = Math.max(nums2[i],max);
-        }
-        int[] tree = new int[max+1];
-        for (int i = nums2.length-1; i >= 0; i--) {
-            res.add(0,get(nums2[i]-1,tree));
-            update(nums2[i],tree);
-        }
-        return res;
-    }
-    private int get(int i, int[] tree) {
-        int num = 0;
-        while (i > 0) {
-            num +=tree[i];
-            i -= i&(-i);
-        }
-        return num;
-    }
-    private void update(int i, int[] tree) {
-        while (i < tree.length) {
-            tree[i] ++;
-            i += i & (-i);
-        }
-    }
+   public List<Integer> countSmaller(int[] nums) {
+        
+	int n = nums.length;
+
+	int[] count = new int[n];
+	int[] temp = new int[n];
+	int[] indices = new int[n];
+
+	for (int i = 0; i < n; i++) 
+		indices[i] = i;
+
+	mergeSort(indices, 0, n - 1, nums, temp, count);
+
+	List<Integer> res = new ArrayList();
+	for (int i = 0; i < n; i++) 
+		res.add(count[i]);
+
+	return res;
+}
+
+private void mergeSort(int[] indices, int start, int end, int[] nums, int[] temp, int[] count) {
+	if (start >= end) return;
+
+	int mid = start + ((end - start) >> 1);
+
+	mergeSort(indices, start, mid, nums, temp, count);
+	mergeSort(indices, mid + 1, end, nums, temp, count);
+	merge(indices, start, mid, end, nums, temp, count);
+
+	return;
+}
+
+private void merge(int[] indices, int start, int mid, int end, int[] nums, int[] temp, int[] count) {
+	int rightCount = 0;
+
+	for (int i = start; i <= end; i++)
+		temp[i] = indices[i];
+
+	for (int left = start, right = mid + 1, k = start; left <= mid || right <= end;) {
+		if (left > mid || right <= end && nums[temp[left]] > nums[temp[right]]) {
+			rightCount++;
+			indices[k++] = temp[right++];
+		} else {
+			count[temp[left]] += rightCount;
+			indices[k++] = temp[left++];
+		}
+	}
+	return;
+}
 }
