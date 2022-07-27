2891b822b0ec584ca401f8c1d8dbd996c5140e0e
KoushikG@kgs <50265588+koushikgshashidhar@users.noreply.github.com>
2022-07-27 12:31:18 +0530

    Time: 3 ms (5.95%), Space: 42.8 MB (43.96%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -13,14 +13,43 @@
  *     }
  * }
  */
+
+//First way using recursion TC:o(n) with auxillary stack space o(n)
+// class Solution {
+//      TreeNode prev=null;
+//     public void flatten(TreeNode root) {
+//         if(root ==null) return;
+
+//         flatten(root.right);
+//         flatten(root.left);
+
+//         root.right=prev;
+//         root.left=null;
+//         prev=root;    
+//     }
+// }
+
+//son way using Iterative way TC:o(n) with O(n) stack
 class Solution {
      TreeNode prev=null;
     public void flatten(TreeNode root) {
-        if(root ==null) return;
-        flatten(root.right);
-        flatten(root.left);
-        root.right=prev;
-        root.left=null;
-        prev=root;    
+        
+        if(root==null)return;
+        Stack<TreeNode> st= new Stack<TreeNode>();
+        st.add(root);
+        while(!st.isEmpty())
+        {
+            TreeNode cur=st.pop();
+            
+            if(cur.right!=null)st.push(cur.right);
+            if(cur.left!=null)st.push(cur.left);
+            
+            cur.left=null;
+            if(!st.isEmpty())
+            cur.right=st.peek();
+        }
+        
     }
 }
+
+
