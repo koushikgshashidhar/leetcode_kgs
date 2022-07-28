33a8ebab2fe61f9bf9cdabff25b9eb17152e12c0
KoushikG@kgs <50265588+koushikgshashidhar@users.noreply.github.com>
2022-07-28 10:59:44 +0530

    Time: 3 ms (95.30%), Space: 44 MB (49.16%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,41 +1,64 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         
-      HashMap<Character,Integer> hm = new HashMap<Character,Integer>();
-
-        for(int i=0;i<s.length();i++)
+        if(s.length()!=t.length())return false;
+        int []arr= new int[26];
+        
+        for(char c:s.toCharArray())
         {
-                char c=s.charAt(i);
-
-                if(hm.containsKey(c))
-                {
-                    hm.put(c,hm.get(c)+1);
-                }
-                else
-                {
-                    hm.put(c,1);
-                }
-
+            arr[c-'a']++;
+        }
+        for(char c:t.toCharArray())
+        {
+            arr[c-'a']--;
+            if(arr[c-'a']<0)return false;
         }
         
-        HashMap<Character,Integer> hm2 = new HashMap<Character,Integer>();
-
-        for(int i=0;i<t.length();i++)
-        {
-                char c=t.charAt(i);
-
-                if(hm2.containsKey(c))
-                {
-                    hm2.put(c,hm2.get(c)+1);
-                }
-                else
-                {
-                    hm2.put(c,1);
-                }
-
-        }
+        // for(int a:arr)
+        // {
+        //     if(a!=0)return false;
+        // }
         
-        return hm.equals(hm2);
+        return true;
+ 
+        
+        
+        
+//       HashMap<Character,Integer> hm = new HashMap<Character,Integer>();
+
+//         for(int i=0;i<s.length();i++)
+//         {
+//                 char c=s.charAt(i);
+
+//                 if(hm.containsKey(c))
+//                 {
+//                     hm.put(c,hm.get(c)+1);
+//                 }
+//                 else
+//                 {
+//                     hm.put(c,1);
+//                 }
+
+//         }
+        
+//         HashMap<Character,Integer> hm2 = new HashMap<Character,Integer>();
+
+//         for(int i=0;i<t.length();i++)
+//         {
+//                 char c=t.charAt(i);
+
+//                 if(hm2.containsKey(c))
+//                 {
+//                     hm2.put(c,hm2.get(c)+1);
+//                 }
+//                 else
+//                 {
+//                     hm2.put(c,1);
+//                 }
+
+//         }
+        
+//         return hm.equals(hm2);
         
     }
 }
