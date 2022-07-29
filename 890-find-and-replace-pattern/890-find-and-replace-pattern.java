37547f2dc87a4639d011f768c5fae6ffcf8cc49a
KoushikG@kgs <50265588+koushikgshashidhar@users.noreply.github.com>
2022-07-30 00:24:06 +0530

    Time: 1 ms (98.01%), Space: 42.8 MB (69.60%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.java
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.java
@@ -13,22 +13,27 @@ class Solution {
     boolean matches(String word,String pattern)
     {
         
-        char []fir= new char[26];
-        char []sec= new char[26];
+        char []map= new char[26];
+       
         
         for(int i=0;i<word.length();i++)
         {
             char c1=word.charAt(i);
             char c2=pattern.charAt(i);
             
-            if(fir[c1-'a']==0)
-                fir[c1-'a']=c2;
-            
-            if(sec[c2-'a']==0)
-                sec[c2-'a']=c1;
-            
-            if((fir[c1-'a']!=c2) || (sec[c2-'a']!=c1))
-                return false;
+            if(map[c1-'a']==0)
+                map[c1-'a']=c2;
+            if(map[c1-'a']!=c2)return false;
+
+        }
+        boolean [] dup= new boolean[26];
+        
+        for(char c:map)
+        {
+            if(c!=0)
+            {if(dup[c-'a'])return false;
+                dup[c-'a']=true;
+            }
         }
         
         return true;
