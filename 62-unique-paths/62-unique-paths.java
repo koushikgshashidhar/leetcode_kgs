29e0fdad9bb01252f19e3e16c73e9091eaa9d59c
KoushikG@kgs <50265588+koushikgshashidhar@users.noreply.github.com>
2022-08-02 10:49:57 +0530

    Time: 0 ms (100.00%), Space: 41 MB (48.94%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,24 +1,48 @@
+//TC:O(m*n) SC:O(m*n)
+// class Solution {
+//     public int uniquePaths(int m, int n) {
+        
+//         int [][]arr= new int [m][n];
+//         for(int i=0;i<m;i++)
+//         {
+//             arr[i][0]=1;
+//         }    
+//         for(int i=0;i<n;i++)
+//         {
+//             arr[0][i]=1;
+//         } 
+        
+//         for(int i=1;i<m;i++)
+//         {
+//             for(int j=1;j<n;j++)
+//             {
+//                 arr[i][j]=arr[i-1][j]+arr[i][j-1];
+                
+//             }
+//         }
+//         return arr[m-1][n-1];
+//     }
+// }
+
+//TC:O(m*n) SC:O(n)
+
 class Solution {
     public int uniquePaths(int m, int n) {
         
-        int [][]arr= new int [m][n];
-        for(int i=0;i<m;i++)
-        {
-            arr[i][0]=1;
-        }    
+        int []arr= new int [n];
+
+         
         for(int i=0;i<n;i++)
         {
-            arr[0][i]=1;
+            arr[i]=1;
         } 
-        
-        for(int i=1;i<m;i++)
+        for(int j=1;j<m;j++)
         {
-            for(int j=1;j<n;j++)
+            for(int i=1;i<n;i++)
             {
-                arr[i][j]=arr[i-1][j]+arr[i][j-1];
-                
+             arr[i]=arr[i-1]+arr[i];
             }
         }
-        return arr[m-1][n-1];
+        return arr[n-1];
     }
 }
