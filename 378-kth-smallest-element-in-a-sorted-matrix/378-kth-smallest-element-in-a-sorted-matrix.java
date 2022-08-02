c139ce4526c549b1b76e3106f6b97d58ac16a791
KoushikG@kgs <50265588+koushikgshashidhar@users.noreply.github.com>
2022-08-02 23:36:33 +0530

    Time: 12 ms (67.16%), Space: 57.4 MB (31.20%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,30 +1,34 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
         
-        PriorityQueue<Integer>  hp= new PriorityQueue<Integer>(Collections.reverseOrder());
-//         {
-            
-//             public int compare(Integer A,Integer B)
-//             {
-//                 return A-B;
-//             }
-//         };
         
         
-        for(int i=0;i<matrix.length;i++)
-        {
-            
-            for(int j=0;j<matrix[i].length;j++)
-            {
-                
-                hp.add(matrix[i][j]);
-                System.out.println(hp.size());
-                if(hp.size()>k)
-                    System.out.println(hp.poll());
-                
+        //Brute Force
+         int n = matrix.length;
+        int [] arr = new int[n*n];
+        int idx = 0;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                arr[idx++] = matrix[i][j];
             }
         }
-        System.out.println(hp.size());
-        return hp.peek();
+        
+        Arrays.sort(arr);
+        
+        return arr[k - 1];
+//         // 2nd Method TC:O(n*logk)
+//         PriorityQueue<Integer>  hp= new PriorityQueue<Integer>(Collections.reverseOrder());
+//         for(int i=0;i<matrix.length;i++)
+//         {
+            
+//             for(int j=0;j<matrix[i].length;j++)
+//             {
+//                 hp.add(matrix[i][j]);
+//                 if(hp.size()>k)
+//                 hp.poll();      
+//             }
+//         }
+//         System.out.println(hp.size());
+//         return hp.peek();
     }
 }
