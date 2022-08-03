0c5efa701ea531ed043008e2b282a75dbd324b6f
KoushikG@kgs <50265588+koushikgshashidhar@users.noreply.github.com>
2022-08-03 12:35:59 +0530

    Time: 144 ms (50.24%), Space: 126.5 MB (75.04%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.java
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.java
@@ -1,35 +1,65 @@
-public class NumArray {
-    int[] tree;
-    int n;
-
+class NumArray {
+ int []segTree;
+      int n;
     public NumArray(int[] nums) {
-        n = nums.length;
-        tree = new int[n << 1];
-        buildTree(nums);
+          n = nums.length;
+       segTree= new int[n*4];
+        build(nums, 0, n-1, 0);
+    }
+    
+     int build(int []nums, int left, int right, int node) {
+        int middle;
+        int leftSum, rightSum;
+
+        if (left == right) return segTree[node] = nums[left];
+
+        middle = (left + right) / 2;
+        leftSum = build(nums, left, middle, 2 * node + 1);
+        rightSum = build(nums, middle+1, right, 2 * node + 2);
+        return segTree[node] = leftSum + rightSum;
     }
 
-    private void buildTree(int[] nums) {
-        for (int i = n; i < n << 1; i++) {
-            tree[i] = nums[i - n];
-        }
+    public void update(int index, int val) {
+         update(index, val, 0, n-1, 0);
+    }
+  
+    int update(int index, int newValue, int ss, int se, int node) {
+        int leftSum, rightSum;
+        int middle;
 
-        for (int i = n - 1; i > 0; i--) {
-            tree[i] = tree[i << 1] + tree[i << 1 | 1];
-        }
+        if (index < ss || index > se) return segTree[node];
+        if (ss == se) return segTree[node] = newValue;
+
+        middle = (ss + se) / 2;
+        leftSum = update(index, newValue, ss, middle, 2 * node + 1);
+        rightSum = update(index, newValue, middle+1, se, 2 * node + 2);
+         segTree[node] = leftSum + rightSum;
+        return segTree[node];
     }
 
-    void update(int i, int val) {
-        for (tree[i += n] = val; i > 0; i >>= 1) {
-            tree[i >> 1] = tree[i] + tree[i ^ 1];
-        }
+    
+    public int sumRange(int left, int right) {
+         return sumRange(left, right, 0, n-1, 0);
+    }
+      int sumRange(int left, int right, int ss, int se, int node) {
+        int leftSum, rightSum;
+        int middle;
+
+        if (right < ss || left > se) return 0;
+        if (left <= ss && se <= right) return segTree[node];
+
+        middle = (ss + se) / 2;
+        leftSum = sumRange(left, right, ss, middle, 2 * node + 1);
+        rightSum = sumRange(left, right, middle+1, se, 2 * node + 2);
+
+        return leftSum + rightSum;
     }
 
-    public int sumRange(int i, int j) {
-        int ret = 0;
-        for (i += n, j += n; i <= j; i >>= 1, j >>= 1) {
-            if ((i & 1) == 1) ret += tree[i++];
-            if ((j & 1) == 0) ret += tree[j--];
-        }
-        return ret;
-    }
 }
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * obj.update(index,val);
+ * int param_2 = obj.sumRange(left,right);
+ */
