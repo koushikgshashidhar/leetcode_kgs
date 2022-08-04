248f89ac549ec26794b3739e0e6dc883c5f82952
KoushikG@kgs <50265588+koushikgshashidhar@users.noreply.github.com>
2022-08-05 00:01:11 +0530

    Time: 23 ms (94.22%), Space: 54.3 MB (71.10%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.java
+++ 729-my-calendar-i/729-my-calendar-i.java
@@ -1,32 +1,91 @@
-class MyCalendar {
+// class MyCalendar {
 
-        ArrayList<ArrayList<Integer>> al;
-    public MyCalendar() {
+//         ArrayList<ArrayList<Integer>> al;
+//     public MyCalendar() {
         
-        al = new ArrayList<ArrayList<Integer>>();
+//         al = new ArrayList<ArrayList<Integer>>();
+//     }
+    
+//     public boolean book(int start, int end) {
+        
+//         if(al.size()==0)
+//         {
+//             al.add(new ArrayList(Arrays.asList(start,end)));
+            
+//         }
+//         else
+//         {
+//             for(int i=0;i<al.size();i++)
+//             {
+                
+//                  // System.out.println(al.get(i).get(0) +" "+end +" "+ al.get(i).get(1)+" "+start);
+//                 if((al.get(i).get(0)<end  && al.get(i).get(1)>start))
+//                     return false;
+            
+//             }
+            
+//             al.add(new ArrayList(Arrays.asList(start,end)));
+//         }
+//         return true;
+//     }
+// }
+
+class Node{
+    int start,end;
+    Node left;
+    Node right;
+    public Node(int start , int end){
+        this.start=start;
+        this.end=end;
+        left=null;
+        right=null;
+    }
+}
+
+class MyCalendar {
+    
+    Node root ;
+    
+
+    public MyCalendar() {
+        this.root = null;
+        
+    }
+    
+    public boolean insert(Node parent , int s , int e){
+        if (parent.start>=e){
+            if(parent.left==null){
+                parent.left=new Node(s,e);
+                return true;
+            }
+            else{
+                return insert(parent.left,s,e);
+            }
+        }
+        else if (parent.end<=s){
+            if(parent.right==null){
+                parent.right=new Node(s,e);
+                return true;
+            }
+            else{
+                return insert(parent.right,s,e);
+            }
+        }
+        
+            return false;
     }
     
     public boolean book(int start, int end) {
+        if(root == null){
+            root= new Node(start,end);
+            return true;
+        }
+        else{
+            return insert(root,start,end);
+        }
         
-        if(al.size()==0)
-        {
-            al.add(new ArrayList(Arrays.asList(start,end)));
-            
-        }
-        else
-        {
-            for(int i=0;i<al.size();i++)
-            {
-                
-                 // System.out.println(al.get(i).get(0) +" "+end +" "+ al.get(i).get(1)+" "+start);
-                if((al.get(i).get(0)<end  && al.get(i).get(1)>start))
-                    return false;
-            
-            }
-            
-            al.add(new ArrayList(Arrays.asList(start,end)));
-        }
-        return true;
+        
+        
     }
 }
 
