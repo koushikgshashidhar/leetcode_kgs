a171ad0c2777ba2b0358d512a4a2b392248c9d5a
KoushikG@kgs <50265588+koushikgshashidhar@users.noreply.github.com>
2022-08-05 19:04:06 +0530

    Time: 1 ms (92.94%), Space: 41.3 MB (61.50%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.java
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -1,34 +1,34 @@
 class Solution {
-    int [][]dp;
+    int []dp;
     public int combinationSum4(int[] nums, int target) {
         
          
-        dp= new int[nums.length][target+1];
-        for(int []a:dp)
-        Arrays.fill(a,-1);
-        findComb(nums,target,0);
+        dp= new int[target+1];
+     
+        Arrays.fill(dp,-1);
+        findComb(nums,target);
           //for(int []a:dp)
         //System.out.println(Arrays.toString(a));
-        return dp[0][target];
+        return dp[target];
         
     }
     
-     int findComb(int []nums,int tar,int pos)
+     int findComb(int []nums,int tar)
     {
          if(tar<0)return 0;
         if(tar==0)return 1;
          
-         if(dp[pos][tar]!=-1)
-             return dp[pos][tar];
+         if(dp[tar]!=-1)
+             return dp[tar];
          int x=0;
          for(int i=0;i<nums.length;i++)
          {
              
-             x+=findComb(nums,tar-nums[i],i);
+             x+=findComb(nums,tar-nums[i]);
              
          }
          
-         dp[pos][tar]=x;
+         dp[tar]=x;
          return x;
     }
 }
