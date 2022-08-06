bb4ddd5617ae6001bacb8722d0d2fbdf969c972d
KoushikG@kgs <50265588+koushikgshashidhar@users.noreply.github.com>
2022-08-07 00:57:06 +0530

    Time: 82 ms (33.33%), Space: 120.1 MB (11.11%) - LeetHub

--- 2365-task-scheduler-ii/2365-task-scheduler-ii.java
+++ 2365-task-scheduler-ii/2365-task-scheduler-ii.java
@@ -1,19 +1,27 @@
 class Solution {
     public long taskSchedulerII(int[] tasks, int space) {
         
-      HashMap<Integer,Long> hm = new HashMap<Integer,Long>();
-        long res=0l;
+//       HashMap<Integer,Long> hm = new HashMap<Integer,Long>();
+//         long res=0l;
         
-        for(int task: tasks)
-        {
-            if(hm.containsKey(task))
-            {
-                hm.put(task,res=Math.max(res,hm.get(task)+space)+1);
-            }
-            else
-            {
-             hm.put(task,++res);       
-            }
+//         for(int task: tasks)
+//         {
+//             if(hm.containsKey(task))
+//             {
+//                 hm.put(task,res=Math.max(res,hm.get(task)+space)+1);
+//             }
+//             else
+//             {
+//              hm.put(task,res+);       
+//             }
+//         }
+//         return res;
+        
+        Map<Integer, Long> next = new HashMap<>();;
+        long res = 0;
+        for (int a : tasks) {
+            res = Math.max(next.getOrDefault(a, 0L), res + 1);
+            next.put(a, res + space + 1);
         }
         return res;
     }
