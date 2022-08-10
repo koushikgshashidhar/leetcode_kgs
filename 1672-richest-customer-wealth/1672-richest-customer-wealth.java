400d8151bdda0c76c26cde2a3070e112e0986e39
KoushikG@kgs <50265588+koushikgshashidhar@users.noreply.github.com>
2022-08-10 22:14:23 +0530

    Time: 5 ms (6.08%), Space: 43.9 MB (9.39%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max=0;
+        for(int i=0;i<accounts.length;i++)
+        {
+                max=Math.max(max,Arrays.stream(accounts[i]).sum());
+        }
+      return max;  
+    }
+}
