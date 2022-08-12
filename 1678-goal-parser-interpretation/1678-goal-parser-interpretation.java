59fb24217e35960dc15662eecc3b29983cf4f3ab
KoushikG@kgs <50265588+koushikgshashidhar@users.noreply.github.com>
2022-08-13 00:09:01 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (73.19%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,23 +1,31 @@
 class Solution {
     public String interpret(String command) {
-        
-        String res="";
-        for(int i=0;i<command.length();i++)
-        {
-            char ch=command.charAt(i);
+        	StringBuilder S = new StringBuilder();
+	for(int i = 0 ; i < command.length() ; i++){
+		if(command.charAt(i)=='G') S.append('G');
+		if(command.charAt(i)=='('){
+			if(command.charAt(i+1)==')') {S.append('o'); i++;}
+			else{S.append("al"); i = i + 3;}
+		}
+	}
+	return S.toString();
+//         String res="";
+//         for(int i=0;i<command.length();i++)
+//         {
+//             char ch=command.charAt(i);
             
-            if(ch=='G')
-                res+="G";
-            else if(ch==')')
-            {
-                if(command.charAt(i-1)=='(')
-                    res+="o";
-                else
-                res+="al";
-            }
+//             if(ch=='G')
+//                 res+="G";
+//             else if(ch==')')
+//             {
+//                 if(command.charAt(i-1)=='(')
+//                     res+="o";
+//                 else
+//                 res+="al";
+//             }
             
 
-        }
-        return res;
+//         }
+//         return res;
     }
 }
