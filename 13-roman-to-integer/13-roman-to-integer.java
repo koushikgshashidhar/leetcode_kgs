9e1f1e6410eaa6e6cd06fdab34bcc36baca6207f
KoushikG@kgs <50265588+koushikgshashidhar@users.noreply.github.com>
2022-08-15 11:24:17 +0530

    Time: 6 ms (85.69%), Space: 45.4 MB (60.34%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.java
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -1,34 +1,7 @@
 class Solution {
     public int romanToInt(String s) {
         
-//         int [] arr= new int[26];
-//         arr['I'-'A']=1;
-//         arr['V'-'A']=5;
-//         arr['X'-'A']=10;
-//         arr['L'-'A']=50;
-//         arr['C'-'A']=100;
-//         arr['D'-'A']=500;
-//         arr['M'-'A']=1000;
-        
-//         int res=0;
-//         int last=arr[s.charAt(s.length()-1)-'A'];
-//         for(int i=s.length()-1;i>=0;i--)
-//         {
-            
-//             int c=s.charAt(i)-'A';
-            
-//             if(arr[c]>=arr[last])
-//                 res+=arr[c];
-//             else
-//                 res-=arr[c];
-//             last=c;
-            
-//         }
-//         return res;
-        
-           int res=0;
-        int []arr=new int[26];
-        
+        int [] arr= new int[26];
         arr['I'-'A']=1;
         arr['V'-'A']=5;
         arr['X'-'A']=10;
@@ -37,23 +10,50 @@ class Solution {
         arr['D'-'A']=500;
         arr['M'-'A']=1000;
         
-        int temp=(int)(s.charAt(s.length()-1)-'A');
+        int res=0;
+        int last=arr[s.charAt(s.length()-1)-'A'];
         for(int i=s.length()-1;i>=0;i--)
         {
             
-           int c=(int)(s.charAt(i)-'A');
+            int c=s.charAt(i)-'A';
             
-           if(arr[temp]<=arr[c])
-               res=res+arr[c];
+            if(arr[c]>=last)
+                res+=arr[c];
             else
-               res=res-arr[c];
-            
-            temp=c;
+                res-=arr[c];
+            last=arr[c];
             
         }
-        
-        
         return res;
+        
+//            int res=0;
+//         int []arr=new int[26];
+        
+//         arr['I'-'A']=1;
+//         arr['V'-'A']=5;
+//         arr['X'-'A']=10;
+//         arr['L'-'A']=50;
+//         arr['C'-'A']=100;
+//         arr['D'-'A']=500;
+//         arr['M'-'A']=1000;
+        
+//         int temp=(int)(s.charAt(s.length()-1)-'A');
+//         for(int i=s.length()-1;i>=0;i--)
+//         {
+            
+//            int c=(int)(s.charAt(i)-'A');
+            
+//            if(arr[temp]<=arr[c])
+//                res=res+arr[c];
+//             else
+//                res=res-arr[c];
+            
+//             temp=c;
+            
+//         }
+        
+        
+//         return res;
         
     }
 }
