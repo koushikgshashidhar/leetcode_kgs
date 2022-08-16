4a1388a0664efcfaae971838474a05d481cdca05
KoushikG@kgs <50265588+koushikgshashidhar@users.noreply.github.com>
2022-08-16 13:48:25 +0530

    Time: 2 ms (99.66%), Space: 48.4 MB (75.13%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -1,6 +1,20 @@
 class Solution {
     public int firstUniqChar(String s) {
-        int []freq=new int[26];
+   
+        
+         int ans = Integer.MAX_VALUE;
+        for (char i = 'a'; i <= 'z';i++) {
+            int ind = s.indexOf (i);
+            if (ind != -1 && ind == s.lastIndexOf (i))
+                ans = Math.min (ans,ind);
+        }
+        if (ans == Integer.MAX_VALUE)
+            return -1;
+        return ans;
+        
+        
+        
+        /*int []freq=new int[26];
         
         char []arr= s.toCharArray();
         
@@ -17,5 +31,7 @@ class Solution {
         }
         
         return -1;
+        
+        */
     }
 }
