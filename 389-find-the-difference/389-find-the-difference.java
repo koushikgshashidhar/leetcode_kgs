076aa82d895c12253af8e6697be25c031118bd96
KoushikG@kgs <50265588+koushikgshashidhar@users.noreply.github.com>
2022-08-16 23:13:35 +0530

    Time: 1 ms (100.00%), Space: 41 MB (86.49%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.java
+++ 389-find-the-difference/389-find-the-difference.java
@@ -1,5 +1,12 @@
 class Solution {
     public char findTheDifference(String s, String t) {
+     
+       char c = 0;
+        for(char cs : s.toCharArray()) c ^= cs;
+        for(char ct : t.toCharArray()) c ^= ct;
+        return c;  
+        
+        /*
         int res=1;
         int []arr= new int[26];
         for(int i=0;i<s.length();i++)
@@ -18,5 +25,6 @@ class Solution {
                 return (char)('a'+i);
         }
          return 'x';
+        */
     }
 }
