d36b3d16270029bbabb0ba47105f8d44474e87fc
KoushikG@kgs <50265588+koushikgshashidhar@users.noreply.github.com>
2022-08-18 20:46:34 +0530

    Time: 92 ms (55.80%), Space: 100 MB (10.50%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -1,30 +1,31 @@
 class Solution {
-    public int minSetSize(int[] arr) {
-		HashMap<Integer,Integer> map=new HashMap<>();
-		for(int i=0;i<arr.length;i++){
-			map.put(arr[i],map.getOrDefault(arr[i],0)+1);
-		}
-
-		// Create a Freq Arr. Eg : [7,7,7,3,3,3] so freq[3] will store 2 , so I will make a freq arr where each index represents frequency to a no and freq[i] -> how many times that freq is encountered.
-		int [] freqarr=new int[100001];
-		for(int key: map.keySet()){
-			freqarr[map.get(key)]++; 
-		}
-		int size=arr.length;int count=0;
-		for(int i=freqarr.length-1;i>=0 && size>arr.length/2;){ // traverse array in rev order to greedily choose the max frequency
-			if(freqarr[i]==0){
-				i--;
-			}else{ // keep reducing the size till freq[i]==0 -> use up all high freq elements
-				while(freqarr[i]!=0 && size>arr.length/2){
-					size-=i;
-					freqarr[i]--; 
-					count++;
-				}
-				if(size<=arr.length/2) return count;
-				i--;
-			}
-		}
-		return count;
+   public int minSetSize(int[] arr) {
+	Map<Integer, Integer> map = new HashMap<>();
+	ArrayList<Integer>[] list = new ArrayList[arr.length + 1];
+	
+	for (int num : arr) {
+		map.put(num, map.getOrDefault(num, 0) + 1);
 	}
 
+	for (int num : map.keySet()) {
+		int count = map.get(num);
+		if (list[count] == null) {
+			list[count] = new ArrayList<Integer>();
+		}
+		list[count].add(num);
+	}
+	
+	int steps = 0, res = 0;
+	for (int i = arr.length; i > 0; i--) {
+		List<Integer> cur = list[i];
+		if (cur == null || cur.size() == 0) continue;
+		for (int num : cur) {
+			steps += i;
+			res++;
+			if (steps >= arr.length / 2)
+				return res;
+		}
+	}
+	return arr.length;
+}
 }
