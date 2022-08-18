d614275c0aa090225d79590ae0c3da5e2cd62c0c
KoushikG@kgs <50265588+koushikgshashidhar@users.noreply.github.com>
2022-08-18 20:48:12 +0530

    Time: 143 ms (19.76%), Space: 83 MB (72.10%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -1,31 +1,13 @@
 class Solution {
-   public int minSetSize(int[] arr) {
-	Map<Integer, Integer> map = new HashMap<>();
-	ArrayList<Integer>[] list = new ArrayList[arr.length + 1];
-	
-	for (int num : arr) {
-		map.put(num, map.getOrDefault(num, 0) + 1);
-	}
-
-	for (int num : map.keySet()) {
-		int count = map.get(num);
-		if (list[count] == null) {
-			list[count] = new ArrayList<Integer>();
-		}
-		list[count].add(num);
-	}
-	
-	int steps = 0, res = 0;
-	for (int i = arr.length; i > 0; i--) {
-		List<Integer> cur = list[i];
-		if (cur == null || cur.size() == 0) continue;
-		for (int num : cur) {
-			steps += i;
-			res++;
-			if (steps >= arr.length / 2)
-				return res;
-		}
-	}
-	return arr.length;
-}
+  public int minSetSize(int[] arr) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        Arrays.stream(arr).forEach(num -> countMap.put(num, countMap.getOrDefault(num, 0) + 1));
+        List<Integer> countValues = countMap.values().stream().sorted(Comparator.reverseOrder()).toList();
+        
+        int size = arr.length, result = 0;
+        while (size > arr.length / 2) 
+            size -= countValues.get(result++);
+        
+        return result;
+    }
 }
