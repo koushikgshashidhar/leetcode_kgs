a55540675c9ec1df42a8c7370e886a5a9074e960
KoushikG@kgs <50265588+koushikgshashidhar@users.noreply.github.com>
2022-08-24 19:40:18 +0530

    Time: 13 ms (99.78%), Space: 47.7 MB (25.54%) - LeetHub

--- 326-power-of-three/326-power-of-three.java
+++ 326-power-of-three/326-power-of-three.java
@@ -3,6 +3,8 @@ class Solution {
        
         //return n > 0 && 1162261467 % n == 0;
         
-         return Integer.toString(n, 3).matches("^10*$");
+        // return Integer.toString(n, 3).matches("^10*$");
+        
+        return (Math.log10(n) / Math.log10(3)) % 1 == 0;
     }
 }
