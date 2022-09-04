705cccced072ea87d4c75cc6360e498ae1746863
KoushikG@kgs <50265588+koushikgshashidhar@users.noreply.github.com>
2022-09-04 15:02:56 +0530

    Time: 8 ms (18.51%), Space: 42.9 MB (64.55%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.java
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.java
@@ -27,16 +27,20 @@ class Tuple {
 class Solution {
     public List<List<Integer>> verticalTraversal(TreeNode root) {
         
-        
-        List < List < Integer >> list = new ArrayList < > ();
-        list = findVertical(root);
-        return list;
+ 
+        return findVertical(root);
+       
     }
       public static List < List < Integer >> findVertical(TreeNode root) {
+          
         TreeMap < Integer, TreeMap < Integer, PriorityQueue < Integer >>> map = new TreeMap < > ();
+          
         Queue < Tuple > q = new LinkedList < Tuple > ();
+          
         q.offer(new Tuple(root, 0, 0));
+          
         while (!q.isEmpty()) {
+            
             Tuple tuple = q.poll();
             TreeNode node = tuple.node;
             int x = tuple.row;
@@ -49,22 +53,32 @@ class Solution {
             if (!map.get(x).containsKey(y)) {
                 map.get(x).put(y, new PriorityQueue < > ());
             }
+            
             map.get(x).get(y).offer(node.val);
 
             if (node.left != null) {
                 q.offer(new Tuple(node.left, x - 1, y + 1));
             }
+            
             if (node.right != null) {
                 q.offer(new Tuple(node.right, x + 1, y + 1));
             }
+            
         }
+          
         List < List < Integer >> list = new ArrayList < > ();
+          
+          
         for (TreeMap < Integer, PriorityQueue < Integer >> ys: map.values()) {
+            
             list.add(new ArrayList < > ());
-            for (PriorityQueue < Integer > nodes: ys.values()) {
-                while (!nodes.isEmpty()) {
-                    list.get(list.size() - 1).add(nodes.poll());
+            
+            for (PriorityQueue < Integer > pq: ys.values()) {
+               
+                while (!pq.isEmpty()) {
+                    list.get(list.size() - 1).add(pq.poll());
                 }
+                
             }
         }
         return list;
