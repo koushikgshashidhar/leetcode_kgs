60e58120b374ba2f0a13fb537093c3f2705396e2
KoushikG@kgs <50265588+koushikgshashidhar@users.noreply.github.com>
2022-09-14 23:29:20 +0530

    Time: 12 ms (26.17%), Space: 47.7 MB (64.51%) - LeetHub

--- 393-utf-8-validation/393-utf-8-validation.java
+++ 393-utf-8-validation/393-utf-8-validation.java
@@ -1,27 +1,49 @@
 class Solution {
     public boolean validUtf8(int[] data) {
-        	if(data==null || data.length==0) return false;
-	boolean isValid = true;
-	for(int i=0;i<data.length;i++) {
-		if(data[i]>255) return false; // 1 after 8th digit, 100000000
-		int numberOfBytes = 0;
-		if((data[i] & 128) == 0) { // 0xxxxxxx, 1 byte, 128(10000000)
-			numberOfBytes = 1;
-		} else if((data[i] & 224) == 192) { // 110xxxxx, 2 bytes, 224(11100000), 192(11000000)
-			numberOfBytes = 2;
-		} else if((data[i] & 240) == 224) { // 1110xxxx, 3 bytes, 240(11110000), 224(11100000)
-			numberOfBytes = 3;
-		} else if((data[i] & 248) == 240) { // 11110xxx, 4 bytes, 248(11111000), 240(11110000)
-			numberOfBytes = 4;
-		} else {
-			return false;
-		}
-		for(int j=1;j<numberOfBytes;j++) { // check that the next n bytes start with 10xxxxxx
-			if(i+j>=data.length) return false;
-			if((data[i+j] & 192) != 128) return false; // 192(11000000), 128(10000000)
-		}
-		i=i+numberOfBytes-1;
-	}
-	return isValid;
+        
+        
+        int checkNum=0;
+        
+        
+        for(int a:data)
+        {
+            if(checkNum==0)
+            {
+                
+                if((a>>5)==0b110)
+                {
+                    checkNum=1;
+                }
+                else if((a>>4)==0b1110)
+                {
+                    checkNum=2;
+                }
+                else  if((a>>3)==0b11110)
+                {
+                    checkNum=3;
+                }
+                else  if((a>>7)!=0b0)
+                {
+                    return false;
+                }
+                
+                
+            }
+            else
+            {
+                if((a>>6)!=0b10)
+                return false;
+                 checkNum--;
+                
+                
+            }
+   
+            
+        }        
+        if(checkNum==0)
+                return true;
+            else
+                return false;
+        
     }
 }
