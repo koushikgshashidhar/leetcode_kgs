52b9779c16bb67ce8be1f4894755ba32cd333ccf
KoushikG@kgs <50265588+koushikgshashidhar@users.noreply.github.com>
2022-09-15 23:41:39 +0530

    Time: 33 ms (46.83%), Space: 108.7 MB (41.32%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
@@ -14,39 +14,41 @@
  * }
  */
 class Solution {
-    int result = 0;
+    int []arr= new int[9];
+    int res=0;
     public int pseudoPalindromicPaths (TreeNode root) {
-        int[] map = new int[10];
-        findPesudoPalindromUtil(root,map);
-        return result;
-    }
-    
-    void findPesudoPalindromUtil(TreeNode root,int[] map){
-       
-        if(root == null){
-            return;
-        }
-         map[root.val] = map[root.val]+1;
-         if(root.left == null && root.right == null){
-             if(isPalindrome(map))
-                result++;
-        }
-       
-        findPesudoPalindromUtil(root.left,map);
-        findPesudoPalindromUtil(root.right,map);
-        //backtrack
-        map[root.val] = map[root.val]-1;
-       
         
+        
+        dfs(root);
+        return res;
     }
-    boolean isPalindrome(int[] map){
-        int miss = 0;
-        for(int i=0;i<=9;i++){
-            if(map[i] % 2 != 0)
-                miss++;
-            if(miss > 1)
-                return false;
+    boolean isPalindrome()
+    {
+        int flag=0;
+        for(int i=0;i<9;i++)
+        {
+            if(arr[i]%2==1)
+                flag++;
         }
+        if(flag>1)return false;
         return true;
+    }
+    void dfs(TreeNode root)
+    {
+        if(root==null)return;
+        
+        arr[root.val-1]++;
+        
+        if(root.left==null && root.right==null)
+        {
+            if(isPalindrome())
+                res++;
+        }
+        else
+        {
+            dfs(root.left);
+            dfs(root.right);
+        }
+        arr[root.val-1]--;
     }
 }
