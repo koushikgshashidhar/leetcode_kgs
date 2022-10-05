c0aa7531a378f6d15d961c70d9048c84dd83649a
KoushikG@kgs <50265588+koushikgshashidhar@users.noreply.github.com>
2022-10-05 12:39:26 +0530

    Time: 1 ms (88.59%), Space: 43.5 MB (84.63%) - LeetHub

---
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.java
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        
+        if(depth==1)
+        {
+        TreeNode t1= new TreeNode(val,root,null);
+            return t1;
+        } 
+        addLevel(root,val,depth-1);
+        return root;
+    }
+    
+     void addLevel(TreeNode root, int val, int depth)
+    {
+         
+         
+        
+         if(depth==1)
+         {
+            if(root!=null)
+            {//System.out.println(root.val +" "+depth);
+             
+           
+            
+             if(root!=null)
+             {
+                 TreeNode t1= new TreeNode(val,root.left,null);
+                root.left=t1;
+             }
+             if(root!=null)
+             {
+                   TreeNode t2= new TreeNode(val,null,root.right);
+                root.right=t2;
+             }
+             
+            }
+             return;
+         }
+         
+         if(root.left!=null)
+         addLevel(root.left,val,depth-1);
+         if(root.right!=null)
+         addLevel(root.right,val,depth-1);    
+         
+    }
+    
+}
