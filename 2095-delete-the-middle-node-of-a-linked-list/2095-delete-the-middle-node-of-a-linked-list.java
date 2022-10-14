1f4b28f953404bf1063068bcb9932e7d2022adc7
KoushikG@kgs <50265588+koushikgshashidhar@users.noreply.github.com>
2022-10-15 01:40:43 +0530

    Time: 3 ms (100.00%), Space: 63.7 MB (85.64%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -15,24 +15,17 @@ class Solution {
             return null;
         
         ListNode tortise=head;
-        ListNode rabbit=head;
+        ListNode rabbit=head.next.next;
         
-        while(rabbit.next!=null  && rabbit.next.next!=null)
+        while(rabbit!=null  && rabbit.next!=null)
         {
             tortise=tortise.next;
             rabbit=rabbit.next.next;
             
         }
         
-        if(rabbit.next!=null)
-        {
-            tortise.next=tortise.next.next;
-        }
-        else
-        {
-        tortise.val=tortise.next.val;
         tortise.next=tortise.next.next;
-        }
+     
         return head;
         
     }
