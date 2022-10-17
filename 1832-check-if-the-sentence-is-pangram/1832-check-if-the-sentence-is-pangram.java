6f0af10a5ffc886a64b72591dfdba2947b2d1cda
KoushikG@kgs <50265588+koushikgshashidhar@users.noreply.github.com>
2022-10-17 12:34:19 +0530

    Time: 2 ms (70.75%), Space: 42.3 MB (26.59%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,19 +1,22 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
         
-        int []arr=new int [26];
+        // Initialize seen = 0 since we start with no letter.
+        int seen = 0;
         
-        for(char c:sentence.toCharArray())
-        {
-            arr[c-'a']++;
+        // Iterate over 'sentence'.
+        for (char ch : sentence.toCharArray()) {
+            // Map each 'currChar' to its index using its ASCII code.
+            int mappedIndex = ch - 'a';
+
+            // 'currBit' represents the bit for 'currChar'.
+            int currBit = 1 << mappedIndex;
+
+            // Use 'OR' operation since we only add its bit for once.
+            seen |= currBit;
         }
         
-        for(int a:arr)
-        {
-            if(a==0)
-                return false;
-        }
-        
-        return true;
+        // Once we finish iterating, check if 'seen' contains 26 bits of 1.
+        return seen == (1 << 26) - 1;
     }
 }
