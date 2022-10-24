11a9b99e0360516bb2ec4d0c62653e65a4e9a044
KoushikG@kgs <50265588+koushikgshashidhar@users.noreply.github.com>
2022-10-25 00:34:02 +0530

    Time: 51 ms (48.15%), Space: 54.4 MB (43.75%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,21 +1,31 @@
 class Solution {
-    public int maxLength(List<String> A) {
-         List<Integer> dp = new ArrayList<>();
-        dp.add(0);
-        int res = 0;
-        for (String s : A) {
-            int a = 0, dup = 0;
-            for (char c : s.toCharArray()) {
-                dup |= a & (1 << (c - 'a'));
-                a |= 1 << (c - 'a');
-            }
-            if (dup > 0) continue;
-            for (int i = dp.size() - 1; i >= 0; --i) {
-                if ((dp.get(i) & a) > 0) continue;
-                dp.add(dp.get(i) | a);
-                res = Math.max(res, Integer.bitCount(dp.get(i) | a));
+   private boolean isUnique(String str) {
+        if (str.length() > 26) return false;
+        boolean[] used = new  boolean [26];
+        char[] arr = str.toCharArray();
+        for (char ch : arr) {
+            if (used[ch - 'a']){
+                return false;
+            } else {
+                used[ch -'a'] = true;
             }
         }
-        return res;
+        return true;
+    }
+    public int maxLength(List<String> arr) {
+        List<String> res = new ArrayList<>();
+        res.add("");
+        for (String str : arr) {
+            if (!isUnique(str)) continue;
+            List<String> resList = new ArrayList<>();
+            for (String candidate : res) {
+                String temp = candidate + str;
+                if (isUnique(temp)) resList.add(temp);
+            }
+            res.addAll(resList);
+        }
+        int ans = 0;
+        for (String str : res) ans = Math.max(ans, str.length());
+        return ans;
     }
 }
