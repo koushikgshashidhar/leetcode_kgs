ce2322a6a233524a506b2632bdd7701524dd8b66
KoushikG@kgs <50265588+koushikgshashidhar@users.noreply.github.com>
2022-10-29 00:50:20 +0530

    Time: 32 ms (71.95%), Space: 41.8 MB (95.12%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.java
+++ 0835-image-overlap/0835-image-overlap.java
@@ -1,19 +1,44 @@
 class Solution {
-     public int largestOverlap(int[][] A, int[][] B) {
+     private int shiftRightDown(int[][] A, int[][] B, int xShift, int yShift, int N) {
+//i represents row i.e towards y axis
+//j represents column i.e towards x axis
+        int count = 0;
+        for (int i = yShift; i < N; i++) {
+
+            for (int j = xShift; j < N; j++) {
+                if (A[i][j] == 1 && B[i - yShift][j - xShift] == 1)
+                    count++;
+            }
+        }
+        return count;
+    }
+
+    private int shiftRightUp(int[][] A, int[][] B, int xShift, int yShift, int N) {
+        int count = 0;
+        for (int i = yShift; i < N; i++) {
+            for (int j = 0; j < N - xShift; j++) {
+                if (A[i][j] == 1 && B[i - yShift][j + xShift] == 1)
+                    count++;
+            }
+        }
+        return count;
+    }
+
+    public int largestOverlap(int[][] A, int[][] B) {
+        int maxCount = 0;
         int N = A.length;
-        List<Integer> LA = new ArrayList<>(),  LB = new ArrayList<>();
-        HashMap<Integer, Integer> count = new HashMap<>();
-        for (int i = 0; i < N * N; ++i)
-            if (A[i / N][i % N] == 1)
-                LA.add(i / N * 100 + i % N);
-        for (int i = 0; i < N * N; ++i)
-            if (B[i / N][i % N] == 1)
-                LB.add(i / N * 100 + i % N);
-        for (int i : LA) for (int j : LB)
-                count.put(i - j, count.getOrDefault(i - j, 0) + 1);
-        int res = 0;
-        for (int i : count.values())
-            res = Math.max(res, i);
-        return res;
+        //xShift & yShift will be equivalent to the starting row, col of the sliding matrix over fixed matrix
+        //i.e (1,0) the overlap started between
+        //1st Matrix moving, 2nd fixed
+        for (int yShift = 0; yShift < N; yShift++) {
+            for (int xShift = 0; xShift < N; xShift++) {
+                maxCount = Math.max(maxCount, shiftRightDown(A, B, xShift, yShift, N));
+                maxCount = Math.max(maxCount, shiftRightDown(B, A, xShift, yShift, N));
+                maxCount = Math.max(maxCount, shiftRightUp(A, B, xShift, yShift, N));
+                maxCount = Math.max(maxCount, shiftRightUp(B, A, xShift, yShift, N));
+            }
+        }
+
+        return maxCount;
     }
 }
