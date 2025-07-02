15543a37982ee0785b0815d286eaad524a839f12
KoushikG@kgs <50265588+koushikgshashidhar@users.noreply.github.com>
2025-07-02 21:21:44 +0530

    Time: 24 ms (74.16%), Space: 47.7 MB (6.27%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -3,27 +3,43 @@ class Solution {
         
         
         int []prefix= new int [nums.length];
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        int sum=0;
         
-        prefix[0]=nums[0];
+       // prefix[0]=nums[0];
+      //  hm.put(prefix[0],1);
         int res=0;
-        for(int i=1;i<prefix.length;i++)
-        {
-            prefix[i]=prefix[i-1]+nums[i];
+        // for(int i=1;i<prefix.length;i++)
+        // {
+        //     prefix[i]=prefix[i-1]+nums[i];
             
-        }
+        //   //  hm.put(prefix[i],hm.getOrDefault(prefix[i],0)+1);
+            
+        // }
+        
+        
        // System.out.println(Arrays.toString(prefix));
-         for(int i=0;i<prefix.length;i++)
+       
+         for(int i=0;i<nums.length;i++)
         {
-            
-             for(int j=i+1;j<prefix.length;j++)
-             {
-                if((prefix[j]-prefix[i])==k )
+            sum+=nums[i];
+            if(hm.containsKey(sum-k))
+            {
+                res+=hm.get(sum-k);
+               // hm.put(prefix[i]-k,hm.get(prefix[i]-k));
+                
+            }
+               
+            if(sum==k)
                 res++;
-             }
-            if(prefix[i]==k)
-            res++;
+                
+                  hm.put(sum,hm.getOrDefault(sum,0)+1);
             
         }
+        
+        
+      
+        
         return res;
     }
 }
