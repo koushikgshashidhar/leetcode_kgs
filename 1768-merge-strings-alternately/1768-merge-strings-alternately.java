bc811fc9808e2c97390b54600ba4924be30b4b17
KoushikG@kgs <50265588+koushikgshashidhar@users.noreply.github.com>
2026-08-06 22:37:29 +0530

    Time: 1 ms (96.05%), Space: 42.9 MB (78.38%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,46 +1,18 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        
-//         int i=0,j=0;
-//         StringBuilder sbr= new StringBuilder();
-//         while(i<word1.length() && j<word2.length())
-//         {
-//             sbr.append(word1.charAt(i));
-//             sbr.append(word2.charAt(j));
-//             i++;
-//             j++;
-//         }
-//          while(i<word1.length())
-//         {
-//             sbr.append(word1.charAt(i));
-//              i++;
-          
-//         }
-//          while( j<word2.length())
-//         {
-//             sbr.append(word2.charAt(j));
-//              j++;
-//         }
-        
-//         return sbr.toString();
-         int length1 = word1.length(), length2 = word2.length();
-        
-        char[] resultArr = new char[length1 + length2];
-        
-        // Two pointers are maintained which will be used to 
-        // fetch the characters in alternating order.       
-        int pointer1 = 0, pointer2 = 0, resultPointer = 0;
-        
-        //When either of the word's traversal is completed then loop
-        //will only consider the other word.
-        while (pointer1 < length1 || pointer2 < length2) {
-            if (pointer1 < length1) {
-                resultArr[resultPointer++] = word1.charAt(pointer1++);
-            }
-            if (pointer2 < length2) {
-                resultArr[resultPointer++] = word2.charAt(pointer2++);
-            }
+
+        int w1= word1.length();
+        int w2= word2.length();
+        int x=0;
+        StringBuilder sb= new StringBuilder("");
+        for(int i=0;i<Math.max(w1,w2);i++)
+        {
+            if(i<w1)
+            sb.append(word1.charAt(i));
+
+            if(i<w2)
+            sb.append(word2.charAt(i));
         }
-        return new String(resultArr);
+        return sb.toString();
     }
 }
